8653b9dda84706ff3049375bd18654f7b7bb43d4
Anastasiia Anisimova <anisiana@cvut.cz>
2026-01-24 12:34:36 +0100

    small changes

--- src/main/java/start/service/exception/BusinessException.java
+++ src/main/java/start/service/exception/BusinessException.java
@@ -1,14 +1,5 @@
 package start.service.exception;
 
-/**
- * Exception pro business logiku aplikace.
- * Používá se pro chyby související s business pravidly, jako jsou:
- * - Prázdný košík
- * - Nedostatek zboží na skladě
- * - Neplatný stav objednávky
- * - Neplatný přechod mezi stavy
- * - Objednávka nepatří uživateli
- */
 public class BusinessException extends RuntimeException {
     
     public BusinessException(String message) {
